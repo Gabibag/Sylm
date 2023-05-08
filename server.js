85a089d2012f2f312214c6eaf39bf372c037660a
WilyHyperion <123993989+WilyHyperion@users.noreply.github.com>
2023-05-08 22:10:43 +0000

    Game IDs

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 let app = require('express')()
 const fs = require('fs')
-
-
-/* app.post('api/creategame', function(req, res ){
-
-} );*/
+const GameRunner = require('./GameRunner.js')
+app.post('/api/creategame', function(req, res ){
+    res.send( {'id': GameRunner.NewGame()} );
+} );
+app.post('/api/joingame', function(req, res ){
+  //TODO
+} );
+app.post('/api/leavegame', function(req, res ){
+  //TODO
+} );
+app.get('/games/:id' , function(req, res){
+  let id = req.params.id
+  if (GameRunner.Games[id]){
+    res.send( GameRunner.Games[id] )
+    return;
+  }
+  //TODO: 404
+  res.send( "Game not found" )
+} );
 app.get('/', function (req, res) {
     res.sendFile( __dirname + '/public/pages/index.html')
 });
@@ -14,7 +28,7 @@ app.get('styles/:f', function(req, res){
 
 });
 app.get('javascript/:f', function(req, res){
-  res.sendfile(__dirname + '/public/javascript/' + f);
+  res.sendFile(__dirname + '/public/javascript/' + f);
 
 });
 app.get('/:page', function (req, res) {
