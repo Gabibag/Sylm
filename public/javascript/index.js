96de6ae5d8ca9c4d07827daf16eb8130c2bc0c0b
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-05-11 17:04:17 -0700

    Think im done?

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,7 @@
 let swapToSignin = document.getElementsByClassName('swapToSignin')[0]
 let loginButton = document.getElementsByClassName('loginButton')[0]
 
+
 window.onload = function(){
     let mainTitle = document.getElementsByClassName('mainTitle')[0]
     let pencil = document.getElementsByClassName('pencil')[0]
@@ -11,4 +12,12 @@ function redirectButton(redirect){
     setTimeout(function(){
         window.location.href=redirect
     }, 175)
+}
+
+//when mainTitle is hovered, randomize the random-rotate variable in root
+let mainTitle = document.getElementsByClassName('mainTitle')[0]
+function randomizeRotate(){
+    let randomRotate = Math.round((Math.random() * 3) - 1)
+    let r = document.querySelector(':root');
+    r.style.setProperty('--random-rotate', randomRotate + 'deg');
 }
