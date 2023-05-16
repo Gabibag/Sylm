f79d5d6c563e4b4edc57207c5a497a4480064aa0
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-05-16 10:51:11 -0700

    animation

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,15 @@
-let swapToSignin = document.getElementsByClassName('swapToSignin')[0]
-let loginButton = document.getElementsByClassName('loginButton')[0]
+let buttons = document.getElementsByClassName('buttons')
 
 
 window.onload = function(){
     let mainTitle = document.getElementsByClassName('mainTitle')[0]
     let pencil = document.getElementsByClassName('pencil')[0]
+    //set pencil's transition speed to 0
     pencil.style.transform = 'rotate(' + (Math.atan(mainTitle.offsetHeight/mainTitle.offsetWidth) * 180 / Math.PI) + 'deg)'
+    //set pencil's transition speed back to 0.5s
+    setTimeout(function() {
+        pencil.style.transition = '0.5s cubic-bezier(.73,.11,.48,1.04) transform'
+    }, 10)
 }
 
 function redirectButton(redirect){
@@ -16,15 +20,30 @@ function redirectButton(redirect){
 
 //when mainTitle is hovered, randomize the random-rotate variable in root
 let mainTitle = document.getElementsByClassName('mainTitle')[0]
-function hoverTitle(){
-    /*let randomRotate = (Math.random() * 180) - 90
-    if (randomRotate <= 1 && randomRotate >= -1 ){
-        randomRotate += 1
-    }
-    let r = document.querySelector(':root');
-    r.style.setProperty('--random-rotate', randomRotate + 'deg');*/
+function mouseOverTittle(){
     //when main title is hovered, hide the brackets
-    let brackets = document.getElementsByClassName('')[0]
-    brackets.style.transition = 'opacity 0.5s'
-    brackets.style.opacity = '0'
+    document.getElementsByClassName('bracketLeft')[0].style.transform = 'translateX(-4vw) scale(1)'
+    document.getElementsByClassName('bracketRight')[0].style.transform = 'translateX(4vw) scale(1)'
+    //move the graduation cap up and to the left a bit
+    document.getElementsByClassName('gradHat')[0].style.transform = 'translate(-25%, -25%) scale(1.1)'
+    //move the pencil up and to the right a bit
+    document.getElementsByClassName('pencil')[0].style.transform = 'translate(25%, -25%) scale(1.1)'
+    //move the calculator down and to the left a bit
+    document.getElementsByClassName('calculator')[0].style.transform = 'translate(-25%, 25%) scale(1.1)'
+    //move the book down and to the right a bit
+    document.getElementsByClassName('book')[0].style.transform = 'translate(25%, 25%) scale(1.1)'
+}
+
+function mouseExitTittle(){
+    document.getElementsByClassName('bracketLeft')[0].style.transform = 'translateX(0) scale(1)'
+    document.getElementsByClassName('bracketRight')[0].style.transform = 'translateX(0) scale(1)'
+
+    document.getElementsByClassName('gradHat')[0].style.transform = 'translate(0, 0) scale(1)'
+    //move the pencil up and to the right a bit
+    document.getElementsByClassName('pencil')[0].style.transform = 'translate(0, 0) scale(1)'
+    //move the calculator down and to the left a bit
+    document.getElementsByClassName('calculator')[0].style.transform = 'translate(0,0) scale(1)'
+    //move the book down and to the right a bit
+    document.getElementsByClassName('book')[0].style.transform = 'translate(0,0) scale(1)'
+
 }
