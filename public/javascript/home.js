52427e6f487ed4d7fbd7a88d5452feec33de4d6d
WilyHyperion <123993989+WilyHyperion@users.noreply.github.com>
2023-05-18 03:40:09 +0000

    Pending changes exported from your codespace

---
+++ public/javascript/home.js
@@ -0,0 +1,8 @@
+function setMaker(){
+    window.location.href = "/setmaker";
+}
+document.getElementById("SearchBar").addEventListener("keypress", function(event) {
+    alert("hello");
+    if (event.keyCode === 13) {
+    }
+});
