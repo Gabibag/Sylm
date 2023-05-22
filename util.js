361352c221f6fe5884b5cd7ec6f48357bbad2a6a
WilyHyperion <123993989+WilyHyperion@users.noreply.github.com>
2023-05-22 16:13:28 +0000

    start on leaderboard css + prevent xss

--- util.js
+++ util.js
@@ -1,10 +1,21 @@
 let invaildChars = ["'", '"', '`', ' ', ";", ":", ",", ".", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "="];
 module.exports = {
     separator: "",
-    getAllCookieDict : function(req){
+    espace: function (str) {
+        var r = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;'
+        };
+        return str.replace(/[&<>]/g, function (m) {
+            return r[m] || m;
+        }
+        );
+    },
+    getAllCookieDict: function (req) {
         let cookies = this.getAllCookies(req);
         let cookieObj = {};
-        for(let c of cookies){
+        for (let c of cookies) {
             let split = c.split('=');
             cookieObj[split[0]] = split[1];
         }
